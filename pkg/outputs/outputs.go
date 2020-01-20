eb5f22ae00d691bac7fd1ad651c5dae2a3b6a15d
Erick Daniszewski <edaniszewski@gmail.com>
2020-01-20 09:07:06 -0500

    use built-in watt type instead of custom watts type

--- pkg/outputs/outputs.go
+++ pkg/outputs/outputs.go
@@ -3,17 +3,6 @@ package outputs
 import "github.com/vapor-ware/synse-sdk/sdk/output"
 
 var (
-	// WattsPower describes readings with power (watts) outputs.
-	WattsPower = output.Output{
-		Name:      "watts",
-		Type:      "power",
-		Precision: 3,
-		Unit: &output.Unit{
-			Name:   "watts",
-			Symbol: "W",
-		},
-	}
-
 	// VAPower describes readings with power (volt-ampere) outputs.
 	VAPower = output.Output{
 		Name:      "volt-ampere",

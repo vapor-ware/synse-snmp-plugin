e08d1753bd8fcff634862e5318f4c3ef69a5fd65
MatthewHink <matthew_hink@yahoo.com>
2021-07-08 12:03:07 -0700

    Version 2.1.2. Emit Well Known Alarm Info in scan. Functional Test. Update deps. Fixes https://vaporio.atlassian.net/browse/VIO-1089.

---
+++ pkg/snmp/mibs/ups_mib/ups_alarms_table_test.go
@@ -0,0 +1,44 @@
+package mibs
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// TestUpsAlarmsInfo tests that the device info string for the SNMP OIDs in the
+// upsAlarmsTable is the same as the name in upsWellKnownAlarms (OIDs under
+// .1.3.6.1.2.1.33.1.6.3)
+func TestUpsAlarmsInfo(t *testing.T) {
+
+	var expected = []string{
+		"upsAlarmBateryBad",
+		"upsAlarmOnBattery",
+		"upsAlarmLowBattery",
+		"upsAlarmDepletedBattery",
+		"upsAlarmTempBad",
+		"upsAlarmInputBad",
+		"upsAlarmOutputBad",
+		"upsAlarmOutputOverload",
+		"upsAlarmOnBypass",
+		"upsAlarmBypassBad",
+		"upsAlarmOutputOffAsRequested",
+		"upsAlarmUpsOffAsRequested",
+		"upsAlarmChargerFailed",
+		"upsAlarmUpsOutputOff",
+		"upsAlarmUpsSystemOff",
+		"upsAlarmFanFailure",
+		"upsAlarmFuseFailure",
+		"upsAlarmGeneralFault",
+		"upsAlarmDiagnosticTestFailed",
+		"upsAlarmCommunicationsLost",
+		"upsAlarmAwaitingPower",
+		"upsAlarmShutdownPending",
+		"upsAlarmShutdownImminent",
+		"upsAlarmTestInProgress",
+	}
+
+	// Order matters.
+	assert.True(t, reflect.DeepEqual(expected, upsAlarmsInfo))
+}
